37a5c27960399c32d86ae5b064b1a1162d6439b0
LuckCky <www.aivanov@gmail.com>
2016-06-25 20:11:10 +0300

    comment

--- analytics.py
+++ analytics.py
@@ -1,14 +1,13 @@
-import json
-import httplib2
-from googleapiclient.discovery import build
 from googlelogin.googlelogin import auth
 
 class Get_GA_data():
+    def get_more_data(self, **dct):
+        get_ga_data
 
-    def get_ga_data(self, **dict):
-        dct = auth().ga_login().data().ga().get(**dict)
+    def get_ga_data(self, **dct):
+        dct = auth().ga_login().data().ga().get(**dct)
         self.dct_data = dct.execute()
-        while self.dct_data['nextLink']:
-            get_ga_data(**dict, dict['start_index']=int(self.dct_data['itemsPerPage'])+1)
+        if self.dct_data['nextLink']:
+            get_more_data()
 
         return self.dct_data
